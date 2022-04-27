d150f29adf137105ad45a2d3b48b4fb3700d93f6
Marcus Cvjeticanin <marcus.cvjeticanin@softwerk.se>
2022-04-27 20:40:59 +0200

    Adding toVectorString function (fixed version)

--- typescript/index.ts
+++ typescript/index.ts
@@ -32,6 +32,17 @@ const moduleVars: IModuleSettings = {
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 // eslint-disable-next-line no-extend-native
+let toVectorString = (array) => {
+    if (!moduleVars.crypto.VectorString) {
+        throw new Error('VectorString unavailable');
+    }
+
+    const arr = new moduleVars.crypto.VectorString();
+
+    array.map(elem => arr.push_back(elem));
+
+    return arr;
+};
 /**
  * A class containing the TurtleCoin cryptographic primitive methods that wraps
  * the Node.js native module, the WASM binary, or native JS implementations
@@ -1487,7 +1498,7 @@ async function tryRunFunc (...args: any[]): Promise<any> {
         } else if (moduleVars.crypto[func]) {
             for (let i = 0; i < args.length; i++) {
                 if (Array.isArray(args[i])) {
-                    args[i] = args[i].toVectorString();
+                    args[i] = toVectorString(args[i]);
                 }
             }
 
